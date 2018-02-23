21dd6800e3919eb94fbdb9c5c0cb9636d340b344
Sam Uwog <suwoghiren@cristoreyny.org>
2018-02-23 13:55:17 +0000

    x Added everything

--- js/script.js
+++ js/script.js
@@ -6,12 +6,27 @@
 
 // Document Ready Function. All of your jQuery should go in here. 
 $( document ).ready(function() {
-  
+  $("#submit").click(function() {
+    var userSays = $("#input").val();
+    var WordArray = userSays.split(" ");
+    
+    for (var i = 0; i<WordArray.length; i++){
+       var word = WordArray[i]
+       var preffix = word.slice(0,1)
+       var suffix = word.slice(1, word.length);
+       var result = suffix + preffix + "ay"
+       console.log(result);
+    $("#message").text(userSays + "ay");
+	});
+
+console.log(pig);
 
 
 
 });
 
+
+  
 
 // Create the wordToPigLatin function that takes a word as a parameter and returns a transfromed word. 
 
